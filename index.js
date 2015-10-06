d28b2fd465d8b92cd99a405411e45ef0248f0f78
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2015-10-06 23:01:34 +0200

    Just playing around. Clicks draw squares

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+'use strict';
+
 require('./style.css');
 var domready = require('domready');
 
+let backgroundColor = '#f0f0f0';
+
 domready(() => {
-  
+  let canvas = document.getElementById('canvas'); 
+  let canvasDims = canvas.getBoundingClientRect();
+  let cellWidth = 70;
+  let cellHeight = cellWidth;
+  let ctx = canvas.getContext('2d');
+  let cellStates = {};
+
+  for (let x = 0; x < 25; x ++) {
+    cellStates[x] = 'off';
+  }
+
+  ctx.fillStyle = backgroundColor;
+  ctx.fillRect(0, 0, canvasDims.width, canvasDims.height);
+
+  canvas.addEventListener('click', (e) => {
+    let x = event.clientX;
+    let y = event.clientY;
+    let row = Math.floor(y / cellHeight);
+    let col = Math.floor(x / cellWidth);
+    let cell = row * 5 + col;
+
+    console.log(x, y, cell, row, col, row * cellWidth, col * cellHeight);
+
+    ctx.fillStyle = 'DodgerBlue';
+    ctx.fillRect(col * cellHeight, row * cellWidth, cellWidth, cellHeight);
+  });
 });
