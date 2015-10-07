5ccc5d13f418b884ac3f407e7bec8ef6d70830f5
Markus Hsi-Yang Fritz <markus.h.y.fritz@gmail.com>
2015-10-07 08:37:41 +0200

    Clicking on an active cell resets it

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ require('./style.css');
 var domready = require('domready');
 
 let backgroundColor = '#f0f0f0';
+let col = 'rgb(30, 144, 255)';
+let colHighlight = 'rgba(30, 144, 255, 0.5)';
 
 domready(() => {
   let canvas = document.getElementById('canvas'); 
@@ -21,15 +23,34 @@ domready(() => {
   ctx.fillRect(0, 0, canvasDims.width, canvasDims.height);
 
   canvas.addEventListener('click', (e) => {
-    let x = event.clientX;
-    let y = event.clientY;
-    let row = Math.floor(y / cellHeight);
-    let col = Math.floor(x / cellWidth);
-    let cell = row * 5 + col;
+    let cell = cellData(e.clientX, e.clientY);
+    let fill;
 
-    console.log(x, y, cell, row, col, row * cellWidth, col * cellHeight);
+    if (cellStates[cell.id] === 'on') {
+      fill = backgroundColor;
+      cellStates[cell.id] = 'off';
+    } else {
+      fill = col;
+      cellStates[cell.id] = 'on';
+    }
 
-    ctx.fillStyle = 'DodgerBlue';
-    ctx.fillRect(col * cellHeight, row * cellWidth, cellWidth, cellHeight);
+    ctx.fillStyle = fill;
+    ctx.fillRect(cell.rectX, cell.rectY, cellWidth, cellHeight);
   });
+
+  function cellData(mouseX, mouseY) {
+    let row = Math.floor(mouseY / cellHeight);
+    let col = Math.floor(mouseX / cellWidth);
+    let id = row * 5 + col;
+    let rectX = col * cellWidth;
+    let rectY = row * cellHeight;
+
+    return {
+      row: row,
+      col: col,
+      id: id,
+      rectX: rectX,
+      rectY: rectY
+    };
+  }
 });
